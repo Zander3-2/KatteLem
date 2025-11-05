197f79f97bb6ddd0f0fdb8157ce1f14b7525fd8b
TOKJ <tokj0001@stud.ek.dk>
2025-11-05 10:35:13 +0100

    First Commit - changes

--- src/Cat.java
+++ src/Cat.java
@@ -1,2 +1,6 @@
 public class Cat {
+
+    public String chipId;
+    public boolean isRegistred;
+
 }
--- src/Catflap.java
+++ src/Catflap.java
@@ -1,3 +1,29 @@
 
 public class Catflap {
+
+    private FlapStatus flapStatus;
+
+
+}
+
+    //getMode() / setMode(Mode mode)
+
+
+    //registerCat(String chipId)
+
+
+    //clearRegisteredCats()
+
+
+    //isCatRegistered(String chipId)
+
+
+    //canEnter(String chipId)
+
+
+    //canExit(String chipId)
+
+
+
+
     }
