f21bf1b046e70e2801a8be8db7d21201dd13b8f1
abfa23 <abfa0002@stud.ek.dk>
2025-11-05 10:53:55 +0100

    Mit take på Catflap

--- src/Catflap.java
+++ src/Catflap.java
@@ -1,3 +1,28 @@
-
 public class Catflap {
+    public String chipID;
+    public boolean startMode;
+    public boolean isRegistered;
+
+    public Catflap (String chipID, boolean startMode, boolean isRegistered) {
+        this.chipID = chipID;
+        this.startMode = startMode;
+        this.isRegistered = isRegistered;
+    }
+
+    public boolean getStartMode() {
+        return startMode;
+    }
+
+    public void setStartMode(boolean startMode) {
+        this.startMode = startMode;
+    }
+
+    public boolean getRegisterStatus() {
+        return isRegistered;
+    }
+
+    public void setRegisterStatus(boolean isRegistered) {
+        this.isRegistered = isRegistered;
+    }
+
     }
